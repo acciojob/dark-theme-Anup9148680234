6f2ddacc1ccbea402e26e35947403bf022bce4a3
Anup Angadi <52240005+Anup9148680234@users.noreply.github.com>
2023-02-26 19:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 
 
 function myFunc(){
-	document.getElementById("app").className = "night";
-	document.getElementById("swap").className = "button_night";
+	if(document.getElementById("app").className == "day"){
+		document.getElementById("app").className = "night";
+		document.getElementById("swap").className = "button_night";
+	}
+	else{
+		document.getElementById("app").className = "day";
+		document.getElementById("swap").className = "button_day";
+	}
+	
+	
 }
